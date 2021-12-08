b092d5fe86ee0b0b1d2012307c87edce145e5953
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-08 11:41:20 +0100

    feat: initial version architecture

---
+++ src/services/index.ts
@@ -0,0 +1,40 @@
+import { ApiService } from "./api";
+import { CliService } from "./cli";
+import { ConsensusService } from "./consensus";
+import { GossipService } from "./gossip";
+import { StorageService } from "./storage";
+
+/**
+ * Use the api service.
+ * 
+ * @returns An instance of the api service.
+ */
+export const useApi = () => new ApiService();
+
+/**
+ * Use the cli service.
+ * 
+ * @returns An instance of the cli service.
+ */
+export const useCli = () => new CliService();
+
+/**
+ * Use the consensus service.
+ * 
+ * @returns An instance of the consensus service.
+ */
+export const useConsensus = () => new ConsensusService();
+
+/**
+ * Use the gossip service.
+ * 
+ * @returns An instance of the gossip service.
+ */
+export const useGossip = () => new GossipService();
+
+/**
+ * Use the storage service.
+ * 
+ * @returns An instance of the storage service.
+ */
+export const useStorage = () => new StorageService();
