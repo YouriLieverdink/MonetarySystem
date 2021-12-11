01f0904e899de195e06e3860a05b76cd3894d134
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-11 17:21:12 +0100

    refactor: remove log statement

--- src/services/cli/index.ts
+++ src/services/cli/index.ts
@@ -1,12 +1,4 @@
 import { Service } from 'typedi';
 
 @Service()
-export class CliService {
-
-	private counter = 0;
-
-	public logSomething() {
-		this.counter = this.counter + 1;
-		console.log(`Count: ${this.counter}`);
-	}
-}
+export class CliService {}
