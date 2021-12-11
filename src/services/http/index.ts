6c0b68b8abd36b5ad3e45def7e6d4e0997c82a0b
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-11 16:41:10 +0100

    refactor: rename api to http

--- src/services/http/index.ts
+++ src/services/http/index.ts
@@ -1,4 +1,4 @@
 import { Service } from 'typedi';
 
 @Service()
-export class ApiService {}
+export class HttpService {}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
-export * from './api';
 export * from './cli';
 export * from './consensus';
 export * from './gossip';
+export * from './http';
 export * from './state';
 export * from './storage';
