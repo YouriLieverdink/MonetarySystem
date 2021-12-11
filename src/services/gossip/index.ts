d72da212727376461db492ff2c76728d5af030f6
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-11 18:16:00 +0100

    feat: add ApiService and map endpoints

--- src/services/gossip/index.ts
+++ src/services/gossip/index.ts
@@ -1,4 +1,15 @@
+import { Request, Response } from 'express';
 import { Service } from 'typedi';
 
 @Service()
-export class GossipService {}
+export class GossipService {
+	/**
+	 * Handle incoming requests.
+	 * 
+	 * @param request The received request.
+	 * @param response The object used to send a response.
+	 */
+	public handle(request: Request, response: Response): void {
+		//
+	}
+}
