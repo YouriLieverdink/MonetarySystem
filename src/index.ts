98771cb988735d132ce7dc184ddcb8addab1cd7c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-13 21:49:25 +0100

    feat: create the outline of the StorageService

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
+import Container from 'typedi';
 import { main } from './main';
 import { server } from './server';
+
+Container.set('databasePath', 'db.sqlite3');
 
 main();
 server();
