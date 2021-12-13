38c5072494e31334aecc3f8b6c0a9e22156f8a59
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-12-13 21:50:02 +0100

    refactor: remove unnecessary path registery

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata';
-import Container from 'typedi';
 import { main } from './main';
 import { server } from './server';
-
-Container.set('databasePath', 'db.sqlite3');
 
 main();
 server();
