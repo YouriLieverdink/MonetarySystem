73a1facb1cda25c1532332fe71ad8639dfccff3e
JasperSikkema <jappiesikkeman@gmail.com>
2021-12-16 11:06:17 +0100

    fix: merge conflicts

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,5 +1,6 @@
-import {Database} from 'sqlite3';
-import {Service} from 'typedi';
+import { Database } from 'sqlite3';
+import { Service } from 'typedi';
+import { Address } from '../../types/address';
 
 @Service()
 export class StorageService {
@@ -9,9 +10,123 @@ export class StorageService {
     private database: Database;
 
     /**
+     * The query methods.
+     */
+    private readonly query = {
+        /**
+         * Insert, update, and delete queries.
+         *
+         * @param sql The query.
+         * @returns A promise.
+         */
+        run: (sql: string) => {
+            return new Promise((resolve, reject) => {
+                this.database.run(sql, (err) => {
+                    if (err) reject(err);
+
+                    resolve(true);
+                });
+            });
+        },
+        /**
+         * Read single row.
+         *
+         * @param sql The query.
+         * @param params Parameters used in the query.
+         * @returns A promise.
+         */
+        get: (sql: string, ...params: unknown[]) => {
+            return new Promise((resolve, reject) => {
+                if (params === undefined) params = [];
+
+                this.database.get(sql, params, (err, res) => {
+                    if (err) reject(err);
+
+                    resolve(res);
+                });
+            });
+        },
+        /**
+         * All rows.
+         *
+         * @param sql The query.
+         * @param params Parameters used in the query.
+         * @returns A promise.
+         */
+        all: (sql: string, ...params: unknown[]) => {
+            return new Promise((resolve, reject) => {
+                if (params === undefined) params = [];
+
+                this.database.all(sql, params, (err, res) => {
+                    if (err) reject(err);
+
+                    resolve(res);
+                });
+            });
+        },
+    };
+
+    /**
      * The address methods.
      */
-    public readonly addresses = {};
+    public readonly addresses = {
+        /**
+         * Display a listing of the resource.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        index: async (): Promise<Address[]> => {
+
+            throw Error('Not implemented');
+        },
+        /**
+         * Display the specified resource.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        read: (publicKey: string): Address => {
+
+            throw Error('Not implemented');
+        },
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param privateKey The private key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        create: (publicKey: string, privateKey: string, isDefault = true): void => {
+
+            throw Error('Not implemented');
+        },
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        update: (publicKey: string, isDefault: boolean): void => {
+
+            throw Error('Not implemented');
+        },
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        destroy: (publicKey: string): void => {
+
+            throw Error('Not implemented');
+        },
+    };
 
     /**
      * The events methods.
@@ -21,14 +136,7 @@ export class StorageService {
     /**
      * The node methods.
      */
-    public readonly nodes = {
-        getHostByName(service, name): any {
-            return service.all(
-                `SELECT * FROM nodes WHERE name = ?`,
-                [name]);
-        }
-
-    };
+    public readonly nodes = {};
 
     /**
      * The state methods.
@@ -41,9 +149,6 @@ export class StorageService {
     constructor() {
         this.database = new Database('db.sqlite3');
         this.constructTables();
-        this.nodes.getHostByName(this,'henk').then((value) => {
-            console.log(value)
-        })
     }
 
     /**
@@ -84,19 +189,4 @@ export class StorageService {
 			)
 		`);
     }
-
-    all(sql, params = []) {
-        return new Promise((resolve, reject) => {
-            this.database.all(sql, params, (err, rows) => {
-                if (err) {
-                    console.log('Error running sql: ' + sql)
-                    console.log(err)
-                    reject(err)
-                } else {
-                    resolve(rows)
-                }
-            })
-        })
-    }
 }
-
