a4044ccf465a95ea70178199ffb39b29f1910bce
JasperSikkema <jappiesikkeman@gmail.com>
2021-12-17 00:47:46 +0100

    feat: database crud

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,6 +1,10 @@
-import { Database } from 'sqlite3';
-import { Service } from 'typedi';
-import { Address } from '../../types/address';
+import {Database} from 'sqlite3';
+import {Service} from 'typedi';
+import {Address} from '../../types/address';
+import {Event} from '../../types/event';
+import {Node} from '../../types/node';
+import {State} from '../../types/state';
+
 
 @Service()
 export class StorageService {
@@ -15,7 +19,7 @@ export class StorageService {
     private readonly query = {
         /**
          * Insert, update, and delete queries.
-         * 
+         *
          * @param sql The query.
          * @param params Parameters used in the query.
          * @returns A promise.
@@ -31,7 +35,7 @@ export class StorageService {
         },
         /**
          * Read single row.
-         * 
+         *
          * @param sql The query.
          * @param params Parameters used in the query.
          * @returns A promise.
@@ -49,7 +53,7 @@ export class StorageService {
         },
         /**
          * All rows.
-         * 
+         *
          * @param sql The query.
          * @param params Parameters used in the query.
          * @returns A promise.
@@ -73,76 +77,227 @@ export class StorageService {
     public readonly addresses = {
         /**
          * Display a listing of the resource.
-         * 
+         *
          * @throws {Error} When an exception occurs.
          */
         index: async (): Promise<Address[]> => {
-
-            throw Error('Not implemented');
+            return await this.query.all<Address>('SELECT * FROM addresses');
         },
         /**
          * Display the specified resource.
-         * 
+         *
          * @param publicKey The public key of the address.
-         * 
+         *
          * @throws {Error} When an exception occurs.
          */
-        read: (publicKey: string): Address => {
-
-            throw Error('Not implemented');
+        read: async (publicKey: string): Promise<Address> => {
+            return await this.query.get<Address>('SELECT * FROM addresses WHERE public_key = ?', publicKey);
         },
         /**
          * Store a newly created resource in storage.
-         * 
+         *
          * @param publicKey The public key of the address.
          * @param privateKey The private key of the address.
          * @param isDefault Whether the address should be used as default.
-         * 
+         *
          * @throws {Error} When an exception occurs.
          */
         create: (publicKey: string, privateKey: string, isDefault = true): void => {
-
-            throw Error('Not implemented');
+            this.query.run('INSERT INTO addresses VALUES (?, ?, ?)', publicKey, privateKey, isDefault);
         },
         /**
          * Update the specified resource in storage.
-         * 
+         *
          * @param publicKey The public key of the address.
          * @param isDefault Whether the address should be used as default.
-         * 
+         *
          * @throws {Error} When an exception occurs.
          */
         update: (publicKey: string, isDefault: boolean): void => {
-
-            throw Error('Not implemented');
+            this.query.run('UPDATE addresses SET is_default = ? WHERE public_key = ?', isDefault, publicKey);
         },
         /**
          * Remove the specified resource from storage.
-         * 
+         *
          * @param publicKey The public key of the address.
-         * 
+         *
          * @throws {Error} When an exception occurs.
          */
         destroy: (publicKey: string): void => {
-
-            throw Error('Not implemented');
+            this.query.run('DELETE FROM addresses WHERE public_key = ?', publicKey);
         },
     };
 
     /**
      * The events methods.
      */
-    public readonly events = {};
+    public readonly events = {
+        /**
+         * Display a listing of the resource.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        index: async (): Promise<Event[]> => {
+            return await this.query.all<Event>('SELECT * FROM events');
+        },
+        /**
+         * Display the specified resource.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        read: async (id: number): Promise<Event> => {
+            return await this.query.get<Event>('SELECT * FROM events WHERE id = ?', id);
+        },
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param privateKey The private key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        create: (type: string, data: Record<string, unknown>, otherParent: string, selfParent: string, signature: string, date: Date): void => {
+            this.query.run('INSERT INTO events ("type","data","other_parent","self_parent","signature","date") VALUES (?, ?, ?, ?, ?, ?)', type, data, otherParent, selfParent, signature, date);
+        },
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        update: (type: string, data: Record<string, unknown>, otherParent: string, selfParent: string, signature: string, date: Date, id: number): void => {
+            this.query.run('UPDATE events SET type = ?, data = ?, other_parent = ?, self_parent = ?, signature = ?, date = ? WHERE id = ?', type, data, otherParent, selfParent, signature, date, id);
+        },
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param id The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        destroy: (id: number): void => {
+            this.query.run('DELETE FROM events WHERE id = ?', id);
+        },
+    };
 
     /**
      * The node methods.
      */
-    public readonly nodes = {};
+    public readonly nodes = {
+        /**
+         * Display a listing of the resource.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        index: async (): Promise<Node[]> => {
+            return await this.query.all<Node>('SELECT * FROM nodes');
+        },
+        /**
+         * Display the specified resource.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        read: async (host: string): Promise<Node> => {
+            return await this.query.get<Node>('SELECT * FROM nodes WHERE host = ?', host);
+        },
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param privateKey The private key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        create: (host: string, name: string): void => {
+            this.query.run('INSERT INTO nodes VALUES (?, ?)', host, name);
+        },
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        update: (host: string, name: boolean): void => {
+            this.query.run('UPDATE nodes SET name = ? WHERE host = ?', name, host)
+        },
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        destroy: (host: string): void => {
+            this.query.run('DELETE FROM nodes WHERE host = ?', host)
+        },
+    };
 
     /**
      * The state methods.
      */
-    public readonly states = {};
+    public readonly states = {
+        /**
+         * Display a listing of the resource.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        index: async (): Promise<State[]> => {
+            return await this.query.all<State>('SELECT * FROM states')
+        },
+        /**
+         * Display the specified resource.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        read: async (address: string): Promise<State> => {
+            return await this.query.get<State>('SELECT * FROM states WHERE address = ?', address)
+        },
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param privateKey The private key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        create: (address: string, balance: string, date: Date): void => {
+            this.query.run('INSERT INTO states VALUES (?, ?, ?)', address, balance, date)
+        },
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param publicKey The public key of the address.
+         * @param isDefault Whether the address should be used as default.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        update: (address: string, balance: string, date: Date): void => {
+            this.query.run('UPDATE states SET balance = ?, date = ? WHERE address = ?', balance, date, address)
+        },
+        /**
+         * Remove the specified resource from storage.
+         *
+         * @param publicKey The public key of the address.
+         *
+         * @throws {Error} When an exception occurs.
+         */
+        destroy: (address: string): void => {
+            this.query.run('DELETE FROM states WHERE address = ?', address)
+        },
+    };
 
     /**
      * Class constructor.
@@ -166,11 +321,12 @@ export class StorageService {
 
         this.database.run(`
 			CREATE TABLE IF NOT EXISTS events (
-				id INT,
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				type VARCHAR(16),
 				data BLOB,
 				other_parent TEXT,
 				self_parent TEXT,
+				signature TEXT,
 				date DATETIME
 			)
 		`);
