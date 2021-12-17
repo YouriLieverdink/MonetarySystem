86b51cf5b2ddbea29f08ed3a8cebcddf24f63f05
Bart <bartvp@live.nl>
2021-12-17 15:27:22 +0100

    jest fix

--- src/services/cli/index.test.ts
+++ src/services/cli/index.test.ts
@@ -1,4 +1,3 @@
-import 'reflect-metadata'
 import { Container } from 'typedi';
 import { CommandController } from '../../controllers';
 import { commandControllerMock } from '../../controllers/index.mock';
@@ -7,214 +6,182 @@ import { CliService } from './';
 describe ('CliService',() => {
     Container.set<CommandController>(CommandController, commandControllerMock);
     const cliService: CliService = new CliService();
+    const handle = jest.spyOn(cliService, 'handle');
 
     describe('Reject unknown commands', () => {
 
         it('command: \'test\'', () => {
-            let method = jest.fn(cliService.handle);
-            method('test', console);
-            expect(method).toHaveReturnedWith(false);
+            cliService.handle('test');
+            expect(handle).toHaveReturnedWith(false);
         })
     })
 
     describe('Evaluate number of arguments', () => {
 
         describe('command: import', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.import);
-            })
+            const command = 'import';
 
             it('Accept if 1 argument', () => {
-                method(['example_private_key']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} example_private_key`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if > 1 argument', () => {
-                method(['example_private_key', 'additional_argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} example_private_key additional_argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: remove', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.remove);
-            })
+            const command = 'remove';
 
             it('Accept if 1 argument', () => {
-                method(['example_public_key']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} example_public_key`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if > 1 argument', () => {
-                method(['example_public_key', 'additional_argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} example_public_key additional_argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: generate', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.generate);
-            })
+            const command = 'generate';
 
             it('Accept if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if any arguments', () => {
-                method(['argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: list', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.list);
-            })
+            const command ='list';
 
             it('Accept if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Accept if argument is \'--private\'', () => {
-                method(['--private']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} --private`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if arguments are different than \'--private\'', () => {
-                method(['--private','argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} --private argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: transactions', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.transactions);
-            })
+            const command = 'transactions';
 
             it('Accept if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Accept if 1 argument', () => {
-                method(['example_public_key']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} example_public_key`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if > 1 arguments', () => {
-                method(['example_public_key','additional_argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} example_public_key additional_argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: balance', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.balance);
-            })
+            const command = 'balance';
 
             it('Accept if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Accept if 1 argument', () => {
-                method(['example_public_key']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} example_public_key`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Reject if > 1 arguments', () => {
-                method(['example_public_key','additional_argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} example_public_key additional_argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
         })
 
         describe('command: create-transaction', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.createTransaction);
-            })
+            const command = 'create-transaction';
 
             it('Reject if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if < 3 arguments', () => {
-                method(['receiver', '10']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} receiver 10`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if > 3 arguments', () => {
-                method(['sender', 'receiver', '10', 'additional_argument']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} sender receiver 10 additional_argument`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if 3rd argument not Number', () => {
-                method(['sender', 'receiver', 'ten']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} sender receiver ten`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Accept if 3 arguments & last is Number', () => {
-                method(['example_public_key1', 'example_public_key2', '10']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} example_public_key1 example_public_key2 10`);
+                expect(handle).toHaveReturnedWith(true);
             })
         })
 
         describe('command: mirror', () => {
-            let method: Function;
-
-            beforeEach(() => {
-                method = jest.fn(cliService.commands.mirror);
-            })
+            const command = 'mirror';
 
             it('Reject if no arguments', () => {
-                method([]);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(command);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if > 1 arguments', () => {
-                method(['on','off']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} on off`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Reject if invalid argument', () => {
-                method(['enable']);
-                expect(method).toHaveReturnedWith(false);
+                cliService.handle(`${command} enable`);
+                expect(handle).toHaveReturnedWith(false);
             })
 
             it('Accept if argument \'on\'', () => {
-                method(['on']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} on`);
+                expect(handle).toHaveReturnedWith(true);
             })
 
             it('Accept if argument \'off\'', () => {
-                method(['off']);
-                expect(method).toHaveReturnedWith(true);
+                cliService.handle(`${command} off`);
+                expect(handle).toHaveReturnedWith(true);
             })
         })
     })
--- src/services/cli/index.ts
+++ src/services/cli/index.ts
@@ -1,4 +1,4 @@
-import { Container, Inject, Service } from 'typedi';
+import { Container, Service } from 'typedi';
 import { CommandController } from '../../controllers';
 import { State } from '../../types/state';
 import { Transaction } from '../../types/transaction';
@@ -10,7 +10,7 @@ export class CliService {
 	private commandController = Container.get(CommandController);
 
 	/** The response for the output. */
-	static response: Console;
+	private response: Console;
 
 	/**
 	 * Display the welcome message.
@@ -35,96 +35,83 @@ export class CliService {
 	 * @param request The received string.
 	 * @param response The response object.
 	 */
-	public handle(request: string, response: Console): boolean {
+	public handle(request: string, response: Console = console): boolean {
 		const command = request.trim().split(' ');
-		CliService.response = response;
+		this.response = response;
 
 		try {
 			switch (command.shift()) {
 				case 'exit':
 				case 'quit':
-					this.commands.exit();
-					break;
+					return this.commands.exit();
 				case 'clear':
 					response.clear();
 					break;
 				case 'help':
-					this.commands.help();
-					break;
+					return this.commands.help();
 				case 'import':
-					this.commands.import(command);
-					break;
+					return this.commands.import(command);
 				case 'remove':
-					this.commands.remove(command);
-					break;
+					return this.commands.remove(command);
 				case 'generate':
-					this.commands.generate(command);
-					break;
+					return this.commands.generate(command);
 				case 'list':
-					this.commands.list(command);
-					break;
+					return this.commands.list(command);
 				case 'transactions':
-					this.commands.transactions(command);
-					break;
+					return this.commands.transactions(command);
 				case 'balance':
-					this.commands.balance(command);
-					break;
+					return this.commands.balance(command);
 				case 'create-transaction':
-					this.commands.createTransaction(command);
-					break;
+					return this.commands.createTransaction(command);
 				case 'mirror':
-					this.commands.mirror(command);
-					break;
+					return this.commands.mirror(command);
 				case 'default':
-					this.commands.default(command)
-					break;
-
+					return this.commands.default(command)
 				default:
-					response.log(CliService.errorText('Unknown command'));
+					response.log(this.errorText('Unknown command'));
 					return false;
 			}
 		} catch (e: any) {
-			response.log(CliService.errorText(e.message));
+			response.log(this.errorText(e.message));
 			return false;
 		}
-		return true;
 	}
 
 	/** CLI command interpreters */
 	public commands = {
 		import: (args: string[]): boolean => {
 			if (args.length !== 1)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const privateKey: string = args[0];
 			if (this.commandController.addresses.import(privateKey))
-				return CliService.success(`Successfully imported wallet ${privateKey}`);
+				return this.success(`Successfully imported wallet ${privateKey}`);
 			return false;
 		},
 		remove: (args: string[]): boolean => {
 			if (args.length !== 1)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const publicKey: string = args[0];
 
 			if (this.commandController.addresses.remove(publicKey))
-				return CliService.success('Successfully removed keys removed from this device');
+				return this.success('Successfully removed keys removed from this device');
 			return false;
 		},
 		generate: (args: string[]): boolean => {
 			if (args.length !== 0)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const keyPair = this.commandController.addresses.create();
-			return CliService.success(`Generated new key pair! \n  Important Note: Don't lose the private key. No keys no cheese!\n\n  Private key: 	${keyPair.privateKey}\n  Public key: ${keyPair.publicKey}`)
+			return this.success(`Generated new key pair! \n  Important Note: Don't lose the private key. No keys no cheese!\n\n  Private key: 	${keyPair.privateKey}\n  Public key: ${keyPair.publicKey}`)
 		},
 		list: (args: string[]): boolean => {
 			const showPrivateKeys = args.includes('--private');
 
 			if ((args.length !== 1 && showPrivateKeys) || (args.length === 1 && !showPrivateKeys))
-				return CliService.badRequest();
+				return this.badRequest();
 
-			return CliService.success(`Your key(s):\n` + this.commandController.addresses.getAll()
+			return this.success(`Your key(s):\n` + this.commandController.addresses.getAll()
 				.map((address, index) =>
 				`  ${index+1}. Public key: ${address.publicKey}${
 					(showPrivateKeys) ? `		Private key: ${address.privateKey}` : ''
@@ -133,14 +120,14 @@ export class CliService {
 		},
 		transactions: (args: string[]): boolean => {
 			if (args.length > 1)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const transactions: Transaction[] = (args.length === 1)
 				? this.commandController.transactions.getAll(args[0])
 				: this.commandController.addresses.getAll().flatMap(address =>
 					this.commandController.transactions.getAll(address.publicKey));
 
-			return CliService.success(`Found ${transactions.length} transactions${(args.length === 1) ? ` for ${args[0]}`: ''}:\n`
+			return this.success(`Found ${transactions.length} transactions${(args.length === 1) ? ` for ${args[0]}`: ''}:\n`
 				+ transactions.map((tx, index) =>
 					`  ${index+1}. Sender: ${tx.from}	Receiver: ${tx.to}	Amount: ${tx.amount} \n`)
 					.toString().replace(',', '')
@@ -148,14 +135,14 @@ export class CliService {
 		},
 		balance: (args: string[]): boolean => {
 			if (args.length > 1)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const balances: State[] = (args.length === 1)
 				? [this.commandController.balances.get(args[0])]
 				: this.commandController.addresses.getAll().flatMap(address =>
 					this.commandController.balances.get(address.publicKey));
 
-			return CliService.success(`  Total balance: ${balances.reduce((sum, state) => sum + state.amount, 0)} transactions${(args.length === 1) ? ` for ${args[0]}`: ''}:\n`
+			return this.success(`  Total balance: ${balances.reduce((sum, state) => sum + state.amount, 0)} transactions${(args.length === 1) ? ` for ${args[0]}`: ''}:\n`
 				+ balances.map(state =>
 				`  Address: ${state.publicKey}	Balance: ${state.amount} \n`
 				).toString().replace(',', '')
@@ -163,36 +150,36 @@ export class CliService {
 		},
 		createTransaction: (args: string[]): boolean => {
 			if (args.length !== 3 || isNaN(Number(args.slice(-1)[0])))
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const sender: string = args[0];
 			const receiver: string = args[1];
 			const amount: number = parseInt(args[2]);
 
 			if (this.commandController.transactions.create(sender, receiver, amount))
-				return CliService.success('Created transaction!');
+				return this.success('Created transaction!');
 			return false;
 		},
 		mirror: (args: string[]): boolean => {
 			if (args.length !== 1 || !['on', 'off'].includes(args[0]))
-				return CliService.badRequest();
+				return this.badRequest();
 
 			const enabled = args[0] === 'on';
 			return this.commandController.mirror.set(enabled);
 		},
 		default: (args: string[]): boolean => {
 			if (args.length > 1)
-				return CliService.badRequest();
+				return this.badRequest();
 
 			throw new Error('Not implemented')
 		},
 		exit: (): true => {
-			CliService.response.log('exiting...');
+			this.response.log('exiting...');
 			process.exit(0);
 			return true;
 		},
 		help: (): true => {
-			return CliService.success('Commands:' +
+			return this.success('Commands:' +
 				'\n' +
 				'\n  Wallet setup:' +
 				'\n    generate			Generate new key pair' +
@@ -218,15 +205,15 @@ export class CliService {
 		}
 	}
 
-	private static errorText = msg => `  \x1b[31m${msg}\x1b[0m`;
+	private errorText = (msg) => `  \x1b[31m${msg}\x1b[0m`;
 
-	private static badRequest = (): false => {
-		CliService.response?.log(CliService.errorText('wrong syntax') + '\n  Enter \'help\' to display command line options.');
+	private badRequest = (): false => {
+		this.response?.log(this.errorText('wrong syntax') + '\n  Enter \'help\' to display command line options.');
 		return false;
 	}
 
-	private static success = (msg): true => {
-		CliService.response?.log(`  ${msg}`);
+	private success = (msg): true => {
+		this.response?.log(`  ${msg}`);
 		return true;
 	}
 }
