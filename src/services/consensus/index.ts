8a627e4cebc0680880fe0bfbfca1b9d816cf4f93
JasperSikkema <jappiesikkeman@gmail.com>
2021-12-23 10:59:50 +0100

    feat: outline consensus service

--- src/services/consensus/index.ts
+++ src/services/consensus/index.ts
@@ -1,5 +1,7 @@
 import Container, { Service } from 'typedi';
-import { StorageService } from '..';
+import { QueueService, StorageService } from '..';
+import { Event } from '../../types';
+
 
 @Service()
 export class ConsensusService {
@@ -9,17 +11,148 @@ export class ConsensusService {
 	private storage: StorageService;
 
 	/**
+	 * The queue service.
+	 */
+	private queue: QueueService;
+
+	/**
 	 * Class constructor.
 	 */
 	constructor() {
 		this.storage = Container.get(StorageService);
+		this.queue = Container.get(QueueService);
 	}
 
 	/**
 	 * Initiate a new Consensus calculation.
 	 */
 	public doConsensus(): void {
-		//
+		const events: Event[] = this.queue.pop(0);
+		const divided = this.divideRounds(events);
+		const decided = this.decideFame(divided);
+		const ordered = this.findOrder(decided);
+
+		this.updateStates(ordered);
 		throw Error('Not implemented');
 	}
+
+	/**
+	 * Divide all known transactions into rounds
+	 * @param events All events in the queue that
+	 * @returns returns all events with a round number
+	 */
+	private divideRounds(events: Event[]): {'round': number,'event': Event,'witness': boolean} {
+		/**
+		 * Steps for all events in the list:
+		 * 1. Determine event round
+		 * 2. Determine if event is witness
+		 */
+
+		throw Error('Not implemented');
+	}
+
+	/**
+	 *  Decide if the first events by each member in each round (the “witnesses”) are famous or not
+	 *
+	 * @param devided All events divided in a round
+	 * @returns returns all events with a boolean if the event is famous or not
+	 */
+	private decideFame(devided): {'round': number,'event': Event,'witness': boolean, 'famous': boolean} {
+		/**
+		 * Steps for all events that are a witness:
+		 * 1. Vote for every other events
+		 * 2. if super majority, then decide
+		 * 3. else just vote
+		 * 4. if coin round: if super majority vote else flip a coin
+		 */
+
+		throw Error('Not implemented');
+	}
+
+	/**
+	 * Find the total order for the events for which enough information is available
+	 *
+	 * @param decided All events decided if they are famous or not
+	 * @returns returns all events ordered by round and timestamp
+	 */
+	private findOrder(decided): {'round': number,'event': Event,'witness': boolean, 'famous': boolean, 'consensusTimestamp': Date} {
+		/**
+		 * Steps:
+		 * 1. unique famous witnesses assign a consensus timestamp
+		 * 2. timestamp is median of timestamp when each witness received the event
+		 * 3. sort by round
+		 * 4. sort by timestamp
+		 */
+
+		throw Error('Not implemented');
+	}
+
+	/**
+	 * Update all states based on events where consensus over has been reached
+	 *
+	 * @param ordered All events where over consensus has been reached
+	 */
+	private updateStates(ordered): void {
+		/**
+		 * Steps:
+		 * 1. Loop through all events
+		 * 2. Update the right states with the right amount
+		 */
+
+		throw Error('Not implemented');
+	}
+
+	/**
+	 * The events methods.
+	 */
+	public readonly helperFunctions = {
+		/**
+		 * Determines if y is an ancestor of x
+		 *
+		 * @param x event
+		 * @param y event
+		 * @returns boolean true if event y is an ancestor of event x
+		 */
+		ancestor: (x, y): boolean => {
+
+			throw Error('Not implemented');
+		},
+
+		/**
+		 * Determines if event y is a self-ancestor of event x
+		 *
+		 * @param x event
+		 * @param y event
+		 * @returns boolean true if y is a self-ancestor of x
+		 */
+		selfAncestor: (x, y): boolean => {
+
+			throw Error('Not implemented');
+		},
+
+		/**
+		 * Determines if event x can see event y
+		 *
+		 * @param x event
+		 * @param y event
+		 * @returns boolean true if x sees y
+		 */
+		see: (x, y): boolean => {
+
+			throw Error('Not implemented');
+		},
+
+		/**
+		 * Determines if event x can strongly see event y
+		 *
+		 * @param x event
+		 * @param y event
+		 * @param set list of events of a round
+		 * @returns boolean true if x strongly sees y
+		 */
+		stronglySee: (x, y, set): boolean => {
+
+			throw Error('Not implemented');
+		},
+	};
 }
