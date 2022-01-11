5bd268a67d3387f414c2dc40f1e7f2ab38580411
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-01-11 17:05:59 +0100

    refactor: update the gossip service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,6 @@ export * from './api';
 export * from './cli';
 export * from './consensus';
 export * from './gossip';
-export * from './http';
 export * from './queue';
 export * from './storage';
+
